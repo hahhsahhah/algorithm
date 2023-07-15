48ae1f92548e5c048570e87c96ef0333a48dc491
hahhsahhah <3499403707@qq.com>
2023-07-15 11:43:47 +0800

    增加一个c++程序

--- aa.js
+++ aa.js
@@ -1,7 +1,26 @@
-sdhfsjhkfhjhafjkashfdakhfkja
-dsfghskjhafjkasd
-
-sdhfsjhkfhashfdakhfkja修改项目
-hsdkfhasjkdfa
-
-hsakldhfkajshfd
+class Solution {
+    public:
+        int longestString(int x, int y, int z) {
+            int temp=min(x,y);
+            if(x==0&&y==0)
+            {
+              return z*2;
+            }
+            if(x!=0&&y!=0)
+            {
+                if(x==y)
+                {
+                    return (x+y)*2+z*2;
+                }
+                else
+                {
+                    return (temp*2+1)*2+z*2;
+                }
+            }
+            else
+            {
+                return (z+1)*2;
+            }
+            
+        }
+    };
